8f350ec0d0562529d212aeedeb6cb8a581e14217
Kevin Chan <140776786+kevindkchan@users.noreply.github.com>
2025-04-12 17:43:36 -0600

    Change gradient color

--- src/App.jsx
+++ src/App.jsx
@@ -134,7 +134,7 @@ const GenieLandingPage = () => {
       <Header />
       <div className="marble"></div>
       <Prompt />
-      <div className="shadow-entrance relative -mt-32 h-96 w-screen overflow-hidden [mask-image:radial-gradient(50%_50%,white,transparent)] before:absolute before:inset-0 before:bg-[radial-gradient(circle_at_bottom_center,#8350e8,transparent_70%)] before:opacity-40 after:absolute after:-left-1/2 after:top-1/2 after:aspect-[1/0.7] after:w-[200%] after:rounded-[100%] after:border-t after:border-[#7876c566] after:bg-zinc-900">
+      <div className="shadow-entrance relative -mt-32 h-96 w-screen overflow-hidden [mask-image:radial-gradient(50%_50%,white,transparent)] before:absolute before:inset-0 before:bg-[radial-gradient(circle_at_bottom_center,#8E27F7,transparent_70%)] before:opacity-40 after:absolute after:-left-1/2 after:top-1/2 after:aspect-[1/0.7] after:w-[200%] after:rounded-[100%] after:border-t after:border-[#7876c566] after:bg-zinc-900">
         <Sparkles
           density={1200}
           className="shadow-entrance absolute inset-x-0 bottom-0 h-full w-full [mask-image:radial-gradient(50%_50%,white,transparent_85%)]"
